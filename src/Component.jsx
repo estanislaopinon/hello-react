e8dd811c663c4c6430aab8669a66402e68b56f3a
estanislaopinon <estanislaopinon@gmail.com>
2025-03-15 12:48:51 -0300

    react principiante

---
+++ src/Component.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+export default function Component() {
+  const [text, setText] = useState();
+
+  const [update, setUpdate] = useState();
+
+  const textOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const buttonOnClick = () => {
+    setUpdate(text);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={textOnChange} />
+      <button onClick={buttonOnClick}>Actualizar</button>
+      <p>Texto input: {text}</p>
+      <p>Texto actualizado: {update}</p>
+    </div>
+  );
+}
